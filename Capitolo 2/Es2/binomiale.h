783234af72c2973be4ab4f4954a824a3cbaea592
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-17 09:19:08 +0100

    Create binomiale.h

---
+++ Capitolo 2/Es2/binomiale.h
@@ -0,0 +1,10 @@
+/* Author: Matteo Gianferrari
+ * Data: 16/02/2022
+ */
+#ifndef __BINOMIALE_H__
+#define __BINOMIALE_H__
+
+double binomiale_simmetrico(unsigned int n, unsigned int h, unsigned int k);
+static double fattoriale(unsigned int n);
+
+#endif
