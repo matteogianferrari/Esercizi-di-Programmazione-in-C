6c62671eb95005aab61b3de2897af3a707d2cd28
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-20 21:51:56 +0100

    Create formato.h

---
+++ Capitolo 2/Es10/formato.h
@@ -0,0 +1,12 @@
+/* Author: Matteo Gianferrari
+ * Data: 20/02/2022
+ */
+#ifndef __FORMATO_H__
+#define __FORMATO_H__
+
+#define DATE_LEN 10
+
+int is_number(char c);
+int is_date(const char *s);
+
+#endif
