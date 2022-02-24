30d7f046b1aeb0bbc50fb9126079715c3c976085
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-24 14:22:04 +0100

    Create righe.c

---
+++ Capitolo 2/Es13/righe.c
@@ -0,0 +1,30 @@
+/* Author: Matteo Gianferrari
+ * Data: 24/02/2022
+ */
+#include <stdio.h>
+#include "righe.h"
+
+
+/* unsigned int conta_righe(FILE *f)
+ * Parameters:
+ * FILE *f;  puntatore a file
+ *
+ * Purpose:
+ * Conta le righe di un file di testo
+ *
+ * Return:
+ * numero di righe                     
+ *
+ * Notes:
+ * Il contatore parte da 1 per contare l'ultima riga.
+ */
+unsigned int conta_righe(FILE *f){
+	unsigned int count = 1;
+	char c;
+	
+	while((c = getc(f)) != EOF)
+		if(c == '\n')
+			count++;
+	
+	return count;	
+}
