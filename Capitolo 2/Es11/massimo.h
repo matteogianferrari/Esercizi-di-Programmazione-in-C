0092384f56dd8aca0326f99e181723e12a6b80bc
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-24 12:39:04 +0100

    Update massimo.h

--- Capitolo 2/Es11/massimo.h
+++ Capitolo 2/Es11/massimo.h
@@ -1,18 +1,9 @@
 /* Author: Matteo Gianferrari
  * Data: 20/02/2022
- * Implementare la funzione che accetta un puntatore ad una stringa C contenente il nome di un file da aprire in
- * modalita' lettura (testo). Nel file sono contenuti dei numeri interi. La funzione deve leggere tutti i numeri
- * contenuti nel file e ritornare il massimo. Il parametro file_name sara' sempre il nome di un file valido.
  */
-#include <stdio.h>
-#include "massimo.h"
+#ifndef __MASSIMO_H__
+#define __MASSIMO_H__
 
-int main(void){
-	int i;
-	
-	i = cerca_massimo("max1.txt");
-	i = cerca_massimo("max2.txt");
-	i = cerca_massimo("max3.txt");
-	
-	return 0;
-}
+int cerca_massimo(const char *file_name);
+
+#endif
