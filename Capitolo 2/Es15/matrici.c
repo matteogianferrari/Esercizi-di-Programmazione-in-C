e0d5a9df6ce078566780a5539f2d5349e48cfe0b
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-25 08:50:54 +0100

    Update matrici.c

--- Capitolo 2/Es15/matrici.c
+++ Capitolo 2/Es15/matrici.c
@@ -3,6 +3,19 @@
  */
 #include "matrici.h"
 
+/* double det3x3(double *matr)
+ * Parameters:
+ * double *matr;  puntatore ad array di double
+ *
+ * Purpose:
+ * Calcola il determinante di una matrice 3 x 3
+ *
+ * Return:
+ * Determinante della matrice                    
+ *
+ * Notes:
+ * Non sono richiesti controlli di errori.
+ */
 double det3x3(double *matr){
 	return (matr[0] * matr[4] * matr[8]) + (matr[1] * matr[5] * matr[6]) + (matr[2] * matr[3] * matr[7]) - (matr[6] * matr[4] * matr[2]) - (matr[7] * matr[5] * matr[0]) - (matr[8] * matr[3] * matr[1]);
 }
