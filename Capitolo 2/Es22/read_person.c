ce29b3ff117e70d21c7b2cfe94745830e1d0599b
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-02-27 18:38:19 +0100

    Create read_person.c

---
+++ Capitolo 2/Es22/read_person.c
@@ -0,0 +1,28 @@
+/* Author: Matteo Gianferrari
+ * Data: 27/02/2022
+ */
+#include <stdio.h>
+#include "read_person.h"
+
+/* void person_read(FILE *f, struct person *pp)
+ * Parameters:
+ * FILE *f;            puntatore a file
+ * struct person *pp;  puntatore a struttura person
+ *
+ * Purpose:
+ * Legge da un file i dati relativi alla persona e li 
+ * memorizza nella struttura dati
+ *
+ * Return:
+ * void          
+ *
+ * Notes:
+ * Utilizzate fgets con successiva sscanf invece che fscanf
+ * perche' soluzione piu' sicura.
+ */
+void person_read(FILE *f, struct person *pp){
+	char buf[BUF_LEN];
+	
+	fgets(buf, BUF_LEN, f);
+	sscanf(buf, "%s %u", pp->name, &pp->age);
+}
