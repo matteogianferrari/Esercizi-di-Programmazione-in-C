3db8666d35c10aead479acba78b7d692e278a9e8
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-03 10:56:31 +0100

    Create formato.h

---
+++ Capitolo 3/Es7/formato.h
@@ -0,0 +1,12 @@
+/* Author: Matteo Gianferrari
+ * Data: 03/03/2022
+ */
+#ifndef __FORMATO_H__
+#define __FORMATO_H__
+
+#define HEX_MAX_LEN 10
+#define HEX_MIN_LEN 3
+
+int is_hex(const char *s);
+
+#endif
