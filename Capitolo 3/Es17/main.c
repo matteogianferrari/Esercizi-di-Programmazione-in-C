f598079f925d24962476155b335078457991df09
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-07 08:46:52 +0100

    Update main.c

--- Capitolo 3/Es17/main.c
+++ Capitolo 3/Es17/main.c
@@ -1,3 +1,7 @@
+/* Author: Matteo Gianferrari
+ * Data: 06/03/2022
+ * Implementare la funzione che scriva sul file f (passato gia' aperto in modalita' scrittura) una capsula ottagonale di lato n (n sara' sempre maggiore di 0).
+ */
 #include <stdio.h>
 #include "capsula.h"
 
