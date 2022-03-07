ab7159158308caa3398170a3bede51a21b669f4a
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-07 08:45:08 +0100

    Update capsula.c

--- Capitolo 3/Es17/capsula.c
+++ Capitolo 3/Es17/capsula.c
@@ -1,18 +1,52 @@
+/* Author: Matteo Gianferrari
+ * Data: 06/03/2022
+ */
 #include <stdio.h>
 #include "capsula.h"
 
+/* static void ripeti_char(FILE *f, char c, unsigned char n)
+ * Parameters:
+ * FILE *f;	     puntatore a file aperto in scrittura
+ * char c;           carattere da ripetere
+ * unsigned char n;  numero di ripetizioni
+ 
+ * Purpose:
+ * Stampa su file il carattere c n volte
+ *
+ * Return:
+ * void
+ *
+ * Notes:
+ * Nessuna.
+ */
 static void ripeti_char(FILE *f, char c, unsigned char n){
 	while(n-- > 0)
 		putc(c, f);
 }
 
+/* void capsula(FILE *f, unsigned char n)
+ * Parameters:
+ * FILE *f;	     puntatore a file aperto in scrittura
+ * unsigned char n;  dimensione di un lato
+ 
+ * Purpose:
+ * Stampa su file una capsula ottagonale di lato n
+ *
+ * Return:
+ * void
+ *
+ * Notes:
+ * Utilizzata funzione di supporto per semplificare la comprensione.
+ */
 void capsula(FILE *f, unsigned char n){
 	unsigned char i, j;
 	
+	/*Parte piatta superiore*/
 	ripeti_char(f, ' ', n);
 	ripeti_char(f, '_', n);
 	putc('\n', f);
 	
+	/*Parte obliqua superiori*/
 	for(i = 0; i < n; i++){
 		ripeti_char(f, ' ', n - 1 - i);
 		putc('/', f);
@@ -21,6 +55,7 @@ void capsula(FILE *f, unsigned char n){
 		putc('\n', f);
 	}
 	
+	/*Parte verticale*/
 	for(i = 0; i < n; i++){
 		putc('|', f);
 		ripeti_char(f, ' ', n * 3 - 2);
@@ -28,6 +63,7 @@ void capsula(FILE *f, unsigned char n){
 		putc('\n', f);
 	}
 	
+	/*Parte obliqua inferiore*/
 	for(i = n - 1; i > 0; i--){
 		ripeti_char(f, ' ', n - 1 - i);
 		putc('\\', f);
@@ -36,6 +72,7 @@ void capsula(FILE *f, unsigned char n){
 		putc('\n', f);
 	}
 	
+	/*Ultima riga*/
 	ripeti_char(f, ' ', n - 1);
 	putc('\\', f);
 	ripeti_char(f, '_', n);
