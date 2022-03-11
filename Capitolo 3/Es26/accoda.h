5e83c7a65b8b3df36ad5ec73223c0a9e10c7c74a
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-11 10:40:07 +0100

    Create accoda.h

---
+++ Capitolo 3/Es26/accoda.h
@@ -0,0 +1,11 @@
+/* Author: Matteo Gianferrari
+ * Data: 11/03/2022
+ */
+#ifndef __ACCODA_H__
+#define __ACCODA_H__
+
+#define MAX_LEN 255
+
+int accoda_tutti_file(const char *file_nomi, const char *file_out);
+
+#endif
