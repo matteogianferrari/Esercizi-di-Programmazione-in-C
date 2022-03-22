a7a599d99a97a03ff5df2f8fa870632ed429fbbf
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-22 11:33:25 +0100

    Create matrix.h

---
+++ Capitolo 4/ES11/matrix.h
@@ -0,0 +1,15 @@
+/* Author: Matteo Gianferrari
+ * Data: 18/03/2022
+ */
+#ifndef __MATRIX_H__
+#define __MATRIX_H__
+
+struct matrix {
+	size_t rows;
+	size_t cols;
+	double *data;
+};
+
+struct matrix *mat_trasposta(const struct matrix *a);
+
+#endif
