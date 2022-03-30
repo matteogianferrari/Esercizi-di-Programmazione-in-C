0f522d5f6e17550321ee3cfa9b95f355b55d185a
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-03-30 09:57:28 +0200

    Create libri.h

---
+++ Capitolo 4/Es19/libri.h
@@ -0,0 +1,16 @@
+/* Author: Matteo Gianferrari
+ * Data: 30/03/2022
+ */
+#ifndef __LIBRI_H__
+#define __LIBRI_H__
+
+struct libro {
+	unsigned int codice;
+	char titolo[255];
+	unsigned int pagine;
+};
+
+struct libro *read_libri(const char *filename, size_t *pn);
+struct libro *filtra_libri(struct libro *plibri, size_t *pn, const char *cerca);
+
+#endif
