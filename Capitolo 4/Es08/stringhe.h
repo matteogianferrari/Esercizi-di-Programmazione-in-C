62c13fb0b24fa898bf41ca575d4f47a1c32169be
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-04-01 08:03:45 +0200

    Create stringhe.h

---
+++ Capitolo 4/Es08/stringhe.h
@@ -0,0 +1,9 @@
+/* Author: Matteo Gianferrari
+ * Data: 01/04/2022
+ */
+#ifndef __STRINGHE_H__
+#define __STRINGHE_H__
+
+void stringhe_scrivi(const char *filename, const char **vs, size_t n);
+
+#endif
