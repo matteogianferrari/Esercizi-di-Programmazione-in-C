dfa8a20b9cb4186f6185a812f82b465a2be2b964
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-04-07 17:13:11 +0200

    Create stringhe.h

---
+++ Capitolo 4/Es03/stringhe.h
@@ -0,0 +1,9 @@
+/* Author: Matteo Gianferrari
+ * Data: 07/04/2022
+ */
+#ifndef __STRINGHE_H__
+#define __STRINGHE_H__
+
+char *sostituisci(const char *str, const char *vecchia, const char *nuova);
+
+#endif
