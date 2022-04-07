a9f9dbe86aedb2d6462ee838959ca2a408e6a9bf
Matteo Gianferrari <93923514+matteogianferrari@users.noreply.github.com>
2022-04-07 11:11:32 +0200

    Create stringhe.h

---
+++ Capitolo 4/Es05/stringhe.h
@@ -0,0 +1,12 @@
+/* Author: Matteo Gianferrari
+ * Data: 07/04/2022
+ */
+#ifndef __STRINGHE_H__
+#define __STRINGHE_H__
+
+#define NUM_ALTEZZA 3
+#define NUM_LARGHEZZA 3
+
+void accoda_cifra(char **vs, unsigned char n);
+
+#endif
